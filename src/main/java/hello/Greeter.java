37fedca38575fa2110b97192f8714c33101a74d2
Michael Scales <mscales@mindwireless.com>
2018-02-28 17:28:12 -0600

    test

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,55 +1,23 @@
 package hello;
 
 
-
 public class Greeter {
-
-
 
   private String name = "";
 
-
-
-  public String getName() 
-
-  {
-
+  public String getName() {
     return name;
-
   }
 
-
-
-  public void setName(String name) 
-
-  {
-
+  public void setName(String name) {
       this.name = name;
-
   }
 
-
-
-  public String sayHello() 
-
-  {
-
-  	if (name == "") 
-
-    {
-
+  public String sayHello() {
+  	if (name == "") {
        return "Hello!";
-
-    }
-
-    else 
-
-    {
-
+    } else {
        return "Hello " + name + "!";
-
     }
-
   }
-
 }
