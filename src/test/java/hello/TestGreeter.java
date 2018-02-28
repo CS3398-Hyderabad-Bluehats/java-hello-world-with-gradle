08983d2135cde09200ae55899673bf5b4b603771
Michael Scales <mscales@mindwireless.com>
2018-02-28 17:52:41 -0600

    passing build

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -5,26 +5,37 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class TestGreeter {
 
-   private Greeter g;
+    private Greeter g;
 
-   @Before
-   public void setUp() throws Exception {
-      g = new Greeter();
-   }
+    @Before
+    public void setUp() throws Exception {
+        g = new Greeter();
+    }
 
-   @Test
-   public void testGreeterEmpty() {
-      assertEquals(g.getName(),"");
-      assertEquals(g.sayHello(),"Hello!");
-   }
+    @Test
+    public void testGreeterEmpty() {
+        assertEquals(g.getName(), "");
+        assertEquals(g.sayHello(), "Hello!");
+    }
 
-   @Test
-   public void testGreeter() {
-      g.setName("World");
-      assertEquals(g.getName(),"World");
-      assertEquals(g.sayHello(),"Hello World!");
-   }
+    @Test
+    public void testGreeter() {
+        g.setName("World");
+        assertEquals(g.getName(), "World");
+        assertEquals(g.sayHello(), "Hello World!");
+    }
+
+    @Test
+    public void newtestMHSGreeterPass() {
+        g.setName("with love from Texas");
+        assertNotEquals(g.getName(), "with love from Russia");
+        assertEquals(g.sayHello(), "Hello with love from Texas!");
+    }
+
+    // @Test
+    // public void newtestMHSGreeterFail() {
+    //     assertNull(g.getName());
+    // }
 }
